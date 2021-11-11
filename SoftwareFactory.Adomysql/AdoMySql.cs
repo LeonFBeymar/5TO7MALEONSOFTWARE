1e059f69967fcff3221470b0472004d2d26fb7d8
jorge393 <albertotumadre2@gmail.com>
2021-11-11 14:23:58 -0300

    create

---
+++ SoftwareFactory.Adomysql/AdoMySql.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SoftwareFactory.Adomysql
+{
+    public class Class1
+    {
+    }
+}
---
+++ SoftwareFactory.Core/IAdo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace SoftwareFactory.Core
+{
+    public interface IAdo
+    {
+        void AltaCliente(Cliente cliente);
+        void AltaProyecto(Proyecto proyecto);
+        List<Cliente> ObtenerClientes();
+        List<Proyecto> ObtenerProyectos();
+    }
+}
