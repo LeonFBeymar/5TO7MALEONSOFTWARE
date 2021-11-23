19ebb66c76a66962df9a4f482beb6408950b7aaa
jorge393 <albertotumadre2@gmail.com>
2021-11-23 09:01:35 -0300

    add Program

---
+++ SoftwareFactory.GUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftwareFactory.GUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
